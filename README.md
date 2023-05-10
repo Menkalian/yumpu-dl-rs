6d460791a709e234813e41f60d9ab1ecd642a3ba
Kilian Krampf <kilian@krampf.de>
2023-05-10 21:51:10 +0200

    Implement basic runner

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Tools for downloading pdfs from `yumpu.com`
 
 > Note: This may only work with documents that are (freely) available without login.
-> There is (currently) no support for overwriting the authentication.
+> There is no support for overwriting the authentication.
+> Also not all freely available files work. Try your luck.
 
-See the following for more information:
- - [yumpu-dl-lib](yumpu-dl-lib/README.md)
- - [yumpu-dl-cli](yumpu-dl-lib/README.md)
- - [yumpu-dl-gui](yumpu-dl-lib/README.md)
+# How to run
+To run this program you need rust/cargo installed.
+The downloader can be run with this command (execute in the root directory of the repository):
+
+````shell
+   $ cargo run -- <yumpu-url> <output-pdf-file>
+````
+
+An example to call this program is `cargo run -- https://www.yumpu.com/en/document/read/66625223/lebaron-manuals-92en lebaron-manual-92en.pdf`.
--- yumpu-dl-cli/src/main.rs
+++ yumpu-dl-cli/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use std::process::exit;
+use std::sync::Mutex;
+
+use yumpu_dl_lib::{download_yumpu_to_pdf, Logger};
+
+struct SimpleLogger {
+    completed: Mutex<u64>,
+    operations: Mutex<u64>,
+}
+
+impl Logger for SimpleLogger {
+    fn is_initialized(&self) -> bool {
+        let mtx_ops = self.operations.lock().unwrap();
+        *mtx_ops > 0
+    }
+
+    fn set_total_operations(&self, ops: u64) {
+        let mut mtx_ops = self.operations.lock().unwrap();
+        *mtx_ops = ops;
+    }
+
+    fn increment_progression(&self) {
+        let mtx_ops = self.operations.lock().unwrap();
+        let mut mtx_comp = self.completed.lock().unwrap();
+        *mtx_comp += 1;
+        println!("Progress: [{}/{}]", *mtx_comp, *mtx_ops);
+    }
+
+    fn log_message(&self, msg: &str) {
+        println!("Message: {}", msg);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let params: Vec<_> = std::env::args().collect();
+    if params.len() != 3 {
+        println!("This program takes 2 arguments: yumpu-dl-cli <yumpu-url> <target-file>");
+        exit(0);
+    }
+
+    let url = params[1].to_string();
+    let target = PathBuf::from(params[2].as_str());
+    let logger = SimpleLogger { completed: Mutex::new(0), operations: Mutex::new(0) };
+
+    download_yumpu_to_pdf(&url, &target, Option::Some(&logger)).await.unwrap()
 }
